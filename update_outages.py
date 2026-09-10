30e515172d111a93440906d25e0d86c61603abb3
Maniac3255 <ryan.bell3255@gmail.com>
2026-09-10 12:41:11 -0500

    Update update_outages.py

--- update_outages.py
+++ update_outages.py
@@ -1,5 +1,5 @@
 import requests
-import json
+from pyproj import Transformer
 
 url = "https://sce-outage-ags.esriemcs.com/arcgis/rest/services/43/outage/MapServer/0/query"
 
@@ -11,12 +11,22 @@ params = {
 }
 
 response = requests.get(url, params=params)
-
 data = response.json()
 
 print("Outages Found:", len(data["features"]))
 
-print(json.dumps(
-    data["features"][0],
-    indent=2
-))
+outage = data["features"][0]
+
+x = outage["geometry"]["x"]
+y = outage["geometry"]["y"]
+
+transformer = Transformer.from_crs(
+    "EPSG:3857",
+    "EPSG:4326",
+    always_xy=True
+)
+
+lon, lat = transformer.transform(x, y)
+
+print("Latitude:", lat)
+print("Longitude:", lon)
