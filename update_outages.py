c6825fd01190974590716693e19b972da9ef6673
Maniac3255 <ryan.bell3255@gmail.com>
2026-09-10 10:25:55 -0500

    Update update_outages.py

--- update_outages.py
+++ update_outages.py
@@ -2,16 +2,14 @@ import pandas as pd
 
 sites = pd.read_csv("Sites-small.csv")
 
-sites["Status"] = "NORMAL"
+duke = sites[
+    sites["Vendor Name"]
+    .str.contains("Duke", na=False)
+]
 
-# Test outage
-sites.loc[0:4, "Status"] = "OUTAGE"
-
-outages = sites[sites["Status"] == "OUTAGE"]
-
-outages.to_csv(
-    "impacted_sites.csv",
+duke.to_csv(
+    "duke_sites.csv",
     index=False
 )
 
-print(f"Created {len(outages)} outage records")
+print(f"Found {len(duke)} Duke locations")
